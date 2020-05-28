c12d26beccea9dc5e2a482bfffbbdbbe169bedc2
RaulC2803 <raul.casanova.03@gmail.com>
2020-05-28 00:29:47 -0500

    DIseño de Inicio Registros y búsqueda avanzados
    Co-authored-by: Drexlarr <diegodba29@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { BodegaService } from 'src/app/bodega.service';
+import { Categoria } from 'src/app/model/categoria';
+import { ProductoService } from 'src/app/producto.service';
+import { strict } from 'assert';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +12,41 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'Pfind';
+  busqueda:string;
+  logoInicio =  'https://cdn.discordapp.com/attachments/700237020278030396/714972834518270052/online-shopping.png'
+  cat: Categoria[];
+  isLogin : Boolean;
   
-  constructor(private router: Router){}
+  constructor(private router: Router, private bodegaServicio: BodegaService, private productoservicio : ProductoService){}
 
   ngOnInit(): void {
-    this.router.navigate(["inicio"])
+    this.obtenerCategoria();
+    this.router.navigate(["inicio"]);
   }
+
+  marginImg(){
+    if(this.router.url != '/loginUsuario'){
+      return "margin-top: -10px;";
+    }else{
+      return "margin-top: 0;";
+    }
+  }
+
+  isRClienteRoute(){
+    this.isLogin = this.router.url != '/loginUsuario';
+    return this.isLogin;
+  }
+
+  buscar():void{
+    this.router.navigate(["/buscarProducto/1/" + this.busqueda]);
+  }
+
+  obtenerCategoria(): void{
+    this.bodegaServicio.obtenerCategoria().subscribe(
+      data => this.cat = data
+    )
+  }
+
+
 }
 
--- src/app/pagina-inicio/pagina-inicio.component.ts
+++ src/app/pagina-inicio/pagina-inicio.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductoService } from  'src/app/producto.service'
-import { Categoria } from '../model/categoria';
 import { BodegaService } from '../bodega.service';
 
 @Component({
@@ -11,26 +10,10 @@ import { BodegaService } from '../bodega.service';
 })
 export class PaginaInicioComponent implements OnInit {
 
-  imagen = 'https://cdn.discordapp.com/attachments/476130359545823233/712005957915508862/music-and-multimedia.png'
-  busqueda: string;
-  cat: Categoria[];
 
   constructor(private router:Router, private productoservicio: ProductoService, private bodegaServicio: BodegaService) { }
 
   ngOnInit(): void {
-    this.obtenerCategoria();
-  }
-
-  buscar():void{
-    this.productoservicio.listarProductos(this.busqueda).subscribe(
-      data => console.log(data)
-    )
-  }
-
-  obtenerCategoria(): void{
-    this.bodegaServicio.obtenerCategoria().subscribe(
-      data => this.cat = data
-    )
   }
 
 }

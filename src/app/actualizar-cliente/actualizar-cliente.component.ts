53bb17a2229984d832d96a976abeaf4098a2de30
Rafitipiti <50728246+Rafitipiti@users.noreply.github.com>
2020-06-25 23:34:59 -0500

    Update actualizar-cliente.component.ts

--- src/app/actualizar-cliente/actualizar-cliente.component.ts
+++ src/app/actualizar-cliente/actualizar-cliente.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ClienteService } from '../cliente.service';
+import { Router } from '@angular/router';
+import { Cliente } from '../model/cliente';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-actualizar-cliente',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./actualizar-cliente.component.css']
 })
 export class ActualizarClienteComponent implements OnInit {
+  cliente: Cliente = new Cliente();
+  fileName: String;
+  label_imagen : String = "Selecciona tu imágen de perfil";
+  selectedFile: File;
+  retrievedImage: any;
+  base64Data: any;
+  retrieveResonse: any;
+  ok: Boolean = false;
 
-  constructor() { }
+  constructor(private clienteService: ClienteService, private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  save(){
+    this.cliente.distrito = "Rimac";
+    this.clienteService.actualizarCliente(this.cliente).subscribe(data => {
+      const uploadImageData = new FormData();
+      console.log(this.selectedFile);
+      uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
+      this.clienteService.subirImagen(uploadImageData);
+      this.router.navigate(["/loginCliente"]);
+    });
+  }
+  onFileSelected(event){
+    
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.fileName = event.target.files[0].name;
+      this.selectedFile = event.target.files[0];
+    }
+
+    if(this.fileName.length < 30){
+      this.label_imagen = "Imagen seleccionada: " + this.fileName;
+    }else{
+      if(this.fileName.length > 50){
+        this.label_imagen = "Imagen seleccionada"
+      }
+    }
   }
 
 }

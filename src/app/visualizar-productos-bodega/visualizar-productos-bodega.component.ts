7ae3800d026038f8e2cd304e9de1e14844e69839
Helsac <jonathanvillegas202000@gmail.com>
2020-07-01 21:19:37 -0500

    nuevo componente

---
+++ src/app/visualizar-productos-bodega/visualizar-productos-bodega.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Bodega } from '../model/bodega';
+import { AuthService } from '../auth.service';
+import { ActivatedRoute } from '@angular/router';
+import { BodegaService } from '../bodega.service';
+
+@Component({
+  selector: 'app-visualizar-productos-bodega',
+  templateUrl: './visualizar-productos-bodega.component.html',
+  styleUrls: ['./visualizar-productos-bodega.component.css']
+})
+export class VisualizarProductosBodegaComponent implements OnInit {
+  bod: Bodega;
+  constructor(private bodegaService: BodegaService, private _ActivatedRoute : ActivatedRoute, private authService : AuthService) { }
+
+  ngOnInit(): void {
+    this.bodegaService.buscarBodega(this.authService.usuario.idEntity).subscribe(data =>{
+      console.log(this.bod = data)
+    } );
+
+  }
+  
+}
